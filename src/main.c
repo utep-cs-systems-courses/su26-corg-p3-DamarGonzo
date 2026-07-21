d560b0a53c5e482f2a524da2a97d4ada88c8db9a
DamarGonzo <294263315+DamarGonzo@users.noreply.github.com>
2026-07-20 22:06:23 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,23 @@
+#include <msp430.h>
+#include <lcdutils.h>
+#include <lcddraw.h>
+#include <libTimer.h>
 
+#include "switches.h"
+#include "stateMachines.h"
+
+int main()
+{
+    configureClocks();
+    lcd_init();
+
+    switch_init();
+
+    clearScreen(COLOR_BLACK);
+
+    enableWDTInterrupts();
+
+    or_sr(0x18);
+
+    while(1){}
+}
